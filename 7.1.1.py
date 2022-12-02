af2521a5d3539dce43dfdfcab181a306ecdb41b2
yagnesh45300 <103107128+yagnesh45300@users.noreply.github.com>
2022-12-02 14:07:04 +0530

    Add files via upload

---
+++ 7.1.1.py
@@ -0,0 +1,14 @@
+#create tupple
+t=("yagnesh",5,5.2,"abc")
+#print tupple
+print(t)
+#convert into list
+l=list(t)
+print(l)
+#delet data item from list
+del l[2]
+print(l)
+#convert list into tuplle
+T=tuple(l)
+#print tuplle elements
+print(T)
