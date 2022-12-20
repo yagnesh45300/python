0b73d32fdfe930d20656e55d90a610c8aa08da91
yagnesh45300 <103107128+yagnesh45300@users.noreply.github.com>
2022-12-20 12:32:52 +0530

    Add files via upload

--- 10.5.py
+++ 10.5.py
@@ -1,2 +1,7 @@
-c=input("enter six digit RGB color code : ")
- 
+def yagnesh(h):
+    rgb=[]
+    for i in (0,2,4):
+        de=int(h[i:i+2],16)
+        rgb.append(de)
+    return tuple(rgb)
+print(yagnesh('FF6347'))
